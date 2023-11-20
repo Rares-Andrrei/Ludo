ca0b93a1cda7e65e547878f8b7410d51475a10fe
Tabacaru Rares Andrei <rares.andrei.tabacaru@gmail.com>
2023-11-20 13:06:25 +0200

    Added player class
    Added class for player

---
+++ Ludo/Model/Player.cs
@@ -0,0 +1,17 @@
+﻿namespace Ludo.Model
+{
+    public class Player
+    {
+        public enum PlayerColor
+        {
+            Blue,
+            Green,
+            Red,
+            Yellow,
+        }
+
+        public string Name { get; set; }
+        public byte RemainingPawns { get; set; }
+        public PlayerColor Color { get; set; }
+    }
+}
